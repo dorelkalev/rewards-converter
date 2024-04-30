0da1634e0a880836225ba937c57742475636f2bb
dorelkalev <158530164+dorelkalev@users.noreply.github.com>
2024-04-29 21:16:50 -0400

    Added RewardValue.java

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+
+    private double cash;
+    private double miles;
+
+    public RewardValue(double cash) {
+
+        this.cash = cash;
+        this.miles = this.cash / 0.0035;
+    }
+
+    public RewardValue(int miles){
+
+        this.miles = miles;
+        this.cash = this.miles * 0.0035;
+
+    }
+
+    public double getCashValue() {return this.cash;}
+    public double getMilesValue() {return this.miles;}
+
+
+
 }
